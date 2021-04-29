7adec3ba388b110a39710df9d8c33ca25f3ea569
Aritra <aet419@gmail.com>
2021-04-29 06:27:15 +0530

    bit of error handeling

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const flash = require('connect-flash');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const createError = require('http-errors');
 
 const app = express();
 
@@ -68,5 +69,21 @@ app.use(function (req, res, next) {
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error', {title: 'Error'});
+});
 
 app.listen(3000, console.log(`Server running on 3000`));
